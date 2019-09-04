74e9aeac7310c2d0498a6dd60df4c143f8ed128d
Steffandeu <shibodeu@outlook.com>
2019-09-04 16:46:08 +0800

    no message

---
+++ src/steffan/deu/offer/No12RomanToInteger.java
@@ -0,0 +1,28 @@
+package steffan.deu.offer;
+
+import java.util.HashMap;
+
+public class No12RomanToInteger {
+    private HashMap<Character,Integer> roman = new HashMap<Character,Integer>(){{
+        put('M', 1000);
+        put('D', 500);
+        put('C', 100);
+        put('L', 50);
+        put('X', 10);
+        put('V', 5);
+        put('I', 1);
+    }
+
+    };
+
+    public int romanToInt(String s) {
+        int res = 0, pre = 0;
+        for(Character c : s.toCharArray()){
+            int cur = roman.get(c);
+            res += cur > pre ? cur - 2 * pre : cur;
+            pre = cur;
+        }
+        return res;
+    }
+
+}
